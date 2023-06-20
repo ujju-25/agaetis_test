479f7eada5f82aa263bfc7e661632b421a5b07a7
ujju-25 <137145617+ujju-25@users.noreply.github.com>
2023-06-20 19:25:57 +0530

    Update agaetis_test.sql

--- agaetis_test.sql
+++ agaetis_test.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 19, 2023 at 03:57 PM
+-- Generation Time: Jun 20, 2023 at 03:50 PM
 -- Server version: 10.4.22-MariaDB
 -- PHP Version: 8.1.1
 
@@ -40,8 +40,17 @@ CREATE TABLE `report` (
   `science_marks` int(3) NOT NULL,
   `history_marks` int(3) NOT NULL,
   `geography_marks` int(3) NOT NULL,
-  `remarks` text NOT NULL
+  `remarks` text NOT NULL,
+  `grade` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `report`
+--
+
+INSERT INTO `report` (`id`, `student_id`, `first_name`, `last_name`, `batch_class`, `email`, `english_marks`, `hindi_marks`, `math_marks`, `science_marks`, `history_marks`, `geography_marks`, `remarks`, `grade`) VALUES
+(8, 1234, 'Ujjwal', 'Dubey', 'B3', 'dubeyujjwal97@gmail.com', 76, 86, 98, 74, 87, 76, 'Testing...', 'Distinction'),
+(9, 256272, 'Ujju', 'Dubey', 'C3', 'dubeyujjwal97@gmail.com', 45, 54, 24, 43, 12, 23, 'Test 2', 'Pass');
 
 --
 -- Indexes for dumped tables
@@ -61,7 +70,7 @@ ALTER TABLE `report`
 -- AUTO_INCREMENT for table `report`
 --
 ALTER TABLE `report`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
